73a926ccdd46d07688d64cecd2d60c79ea58f41e
Ammar Dekkal <dekkal.ammar@hotmail.com>
2023-11-01 11:10:50 -0400

    init:CRUD, register, header

---
+++ pages/Register.php
@@ -0,0 +1,53 @@
+<?php
+include_once('../header/header.php');
+?>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<div class="container-form-register">
+    <form class="row g-3">
+        <div class="col-md-3">
+            <label for="validationDefault01" class="form-label">Full name</label>
+            <input type="text" class="form-control" id="validationDefault01" required>
+        </div>
+        <div class="col-md-3">
+            <label for="validationDefault02" class="form-label">Email</label>
+            <input type="email" class="form-control" id="validationDefault02" required>
+        </div>
+        <div class="col-md-3">
+            <label for="validationDefault02" class="form-label">password</label>
+            <input type="password" class="form-control" id="validationDefault02" required>
+        </div>
+        <div class="col-md-3">
+            <label for="validationDefaultUsername" class="form-label">Username</label>
+            <div class="input-group">
+                <input type="text" class="form-control" id="validationDefaultUsername" aria-describedby="inputGroupPrepend2" required>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <label for="validationDefault03" class="form-label">City</label>
+            <input type="text" class="form-control" id="validationDefault03" required>
+        </div>
+        <div class="col-md-3">
+            <label for="validationDefault04" class="form-label">State</label>
+            <select class="form-select" id="validationDefault04" required>
+                <option selected disabled value="">Choose...</option>
+                <option>...</option>
+            </select>
+        </div>
+        <div class="col-md-3">
+            <label for="validationDefault05" class="form-label">Zip</label>
+            <input type="text" class="form-control" id="validationDefault05" required>
+        </div>
+        <div class="col-12">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
+                <label class="form-check-label" for="invalidCheck2">
+                    Agree to terms and conditions
+                </label>
+            </div>
+        </div>
+        <div class="col-12">
+            <button class="btn btn-primary" type="submit">Submit form</button>
+        </div>
+    </form>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
